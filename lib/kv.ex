9c9ea8964fe5becd616330e9fc893f0de4487822
Alexandra Collins <andracolexa@gmail.com>
2023-10-29 09:48:00 +0100

    initialise project

---
+++ lib/kv.ex
@@ -0,0 +1,18 @@
+defmodule KV do
+  @moduledoc """
+  Documentation for `KV`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KV.hello()
+      :world
+
+  """
+  def hello do
+    "I am the Goddess, I am divine"
+  end
+end
